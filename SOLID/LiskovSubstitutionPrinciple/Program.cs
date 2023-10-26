ecfb692e1d17eea6d103d4b99bc689d02a3ce1a9
Matěj Leifert <matej.leifert@ecmsystem.cz>
2023-10-26 12:14:53 +0200

    LiskovSubstitutionPrinciple

---
+++ SOLID/LiskovSubstitutionPrinciple/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace LiskovSubstitutionPrinciple
+{
+    internal class Program
+    {
+        public static int Area(Rectangle r) => r.Width * r.Height;
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle(2,3);
+            Console.WriteLine($"{r} has area {Area(r)}");
+
+            Rectangle sq = new Square();
+            sq.Width = 2;
+            Console.WriteLine($"{sq} has area {Area(sq)}");
+        }
+    }
+}
