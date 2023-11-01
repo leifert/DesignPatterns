c8b7f2381f77c23abd2bc7e6a9a675022f613ef0
Matěj Leifert <matej.leifert@ecmsystem.cz>
2023-11-01 10:22:51 +0100

    Added Singleton

---
+++ GoF/Creational/Singleton/Singleton/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SingletonDatabase sd1 = SingletonDatabase.GetInstance();
+            SingletonDatabase sd2 = SingletonDatabase.GetInstance();
+
+            if (sd1 == sd2) 
+            {
+                Console.WriteLine("Singleton works, both variables contain the same instance."); ;
+            }
+
+            Console.WriteLine(sd1.GetPopulation("Tokyo")); 
+
+        }
+    }
+}
---
+++ GoF/Creational/Singleton/Singleton/SingletonDatabase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MoreLinq;
+
+namespace Singleton
+{
+    public class SingletonDatabase : IDatabase
+    {
+        private Dictionary<string, int> _capitals;
+        private static SingletonDatabase _instance;
+
+        private SingletonDatabase()
+        {
+            Console.WriteLine("Initializing database");
+
+            var batches = File.ReadAllLines("capitals.txt").Batch(2);
+
+            _capitals = batches.ToDictionary(
+                list=> list.ElementAt(0).Trim(),
+                list => int.Parse(list.ElementAt(1)));
+        }
+
+        public static SingletonDatabase GetInstance()
+        {
+            if(_instance == null)
+            {
+                _instance = new SingletonDatabase();
+            }
+            return _instance;
+        }
+        public int GetPopulation(string name)
+        {
+            return _capitals[name];
+        }
+    }
+}
